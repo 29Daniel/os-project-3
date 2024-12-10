ed29e6039af090120f2c90f1d599b850f74f6c5a
Zarif-Cabrera <135859693+Zarif-Cabrera@users.noreply.github.com>
2024-12-09 19:58:18 -0500

    added data structures stuff

--- list.h
+++ list.h
@@ -1,32 +1,56 @@
+#ifndef LIST_H
+#define LIST_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <pthread.h> // For thread synchronization
 
+// Structure representing a user
 struct node {
-   char username[30];
-   int socket;
-   struct node *next;
+    char username[30];
+    int socket;
+    struct node *next;
 };
 
+// Structure representing a chatroom
 struct room {
     char name[30];             // Room name
     struct node *users;        // Linked list of users in the room
-    struct room *next;         // Pointer to the next room
+    struct room *next;         // Pointer to the next room in the list
 };
+
+// Global mutexes for thread-safe access
+pthread_mutex_t user_mutex;
+pthread_mutex_t room_mutex;
 
 /////////////////// USERLIST //////////////////////////
 
-//insert node at the first location
+// Insert a new user into the linked list at the first location
 struct node* insertFirstU(struct node *head, int socket, char *username);
 
-//find a node with given username
+// Search for a user by username in the linked list
 struct node* findU(struct node *head, char* username);
 
-
+// Remove a user from the user list
+void removeUser(struct node **head, int socket);
 
 /////////////////// ROOMLIST //////////////////////////
 
+// Create a new chat room
 struct room *createRoom(char *name);
+
+// Find a room by name
 struct room *findRoom(struct room *head, char *name);
+
+// Remove a chat room
 void removeRoom(struct room **head, char *name);
+
+// Add a user to a room
+void addUserToRoom(struct room *room, struct node *user);
+
+// Remove a user from a room
+void removeUserFromRoom(struct room *room, struct node *user);
+
+#endif // LIST_H
